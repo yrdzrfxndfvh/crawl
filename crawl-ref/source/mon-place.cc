666464cf9c507110cbe8a6018cbfeb6814d2e972
yrdzrfxndfvh <scratn0b@gmail.com>
2023-12-16 20:00:06 -0500

    third times the charm

--- crawl-ref/source/mon-place.cc
+++ crawl-ref/source/mon-place.cc
@@ -2452,7 +2452,7 @@ static const map<band_type, vector<member_possibilities>> band_membership = {
                                   {MONS_DEMONSPAWN_WARMONGER, 1},
                                   {MONS_DEMONSPAWN_CORRUPTER, 1},
                                   {MONS_DEMONSPAWN_BLACK_SUN, 1},
-                                  {MONS_EXECUTIONER_RIDER, 4}}}},
+                                  {MONS_EXECUTIONER_RIDER, 4}}},
     // for Grunn
     { BAND_DOOM_HOUNDS,         {{{MONS_DOOM_HOUND, 1}}}},
     // for Norris
